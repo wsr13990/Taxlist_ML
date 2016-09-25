23e04ffec7b625f919ee6d7b5bac003c33f795c1
WSR13990 <wahyusejatiroso@yahoo.co.id>
2016-09-25 23:33:01 +0800

    Add company filter 2

--- ML Script.R
+++ ML Script.R
@@ -2,6 +2,7 @@ library(tm)
 library(SnowballC)
 library(C50)
 library(RWeka)
+library(caret)
 
 options(java.parameters = "-Xmx8000m")
 
@@ -30,33 +31,50 @@ convert_count <- function(x){
 }
 
 taxlist <- read.csv("D:/Taxlist_ML/Taxlist.csv", header = TRUE, sep=",")
-taxlist <- taxlist[taxlist$Nama.Vendor=="Huawei Tech Investment",]
+taxlist <- taxlist[taxlist$Nama.Vendor %in% c("Huawei Tech Investment"),]
+# taxlist <- taxlist[taxlist$Nama.Vendor %in% c(
+#   "Huawei Tech Investment",
+#   "Ericsson Indonesia",
+#   "Nokia Solutions and Networks Indonesia",
+#   "Westindo Esa Perkasa",
+#   "Westindo Putra Perkasa",
+#   "Wiraky Nusa Telekomunikasi",
+#   "Hariff Daya Tunggal Engineering",
+#   "Abhimata Citra Abadi",
+#   "INFRASTRUKTUR TELEKOMUNIKASI INDONESIA",
+#   "Consistel Indonesia",
+#   "Ericsson AB",
+#   "Nokia Solutions and Networks Oy",
+#   "Huawei International Pte Ltd",
+#   "Huawei Technologies Co Ltd"),]
 set.seed(123)
-#row_num <- nrow(taxlist)
-#train_num <- round(row_num/2)
-#train_sample <- sample(row_num, train_num)
-#taxlist_train <- taxlist[train_sample,]
-#taxlist_test <- taxlist[-train_sample,]
-#crossval_row <- round(nrow(taxlist_test)/2)
-#taxlist_crossval <- taxlist_test[1:crossval_row,]
-#taxlist_test <- taxlist_test[(crossval_row):nrow(taxlist_test),]
-taxlist_train <- taxlist
+row_num <- nrow(taxlist)
+train_num <- round(row_num/2)
+train_sample <- sample(row_num, train_num)
+taxlist_train <- taxlist[train_sample,]
+taxlist_test <- taxlist[-train_sample,]
+crossval_row <- round(nrow(taxlist_test)/2)
+taxlist_crossval <- taxlist_test[1:crossval_row,]
+taxlist_test <- taxlist_test[(crossval_row):nrow(taxlist_test),]
+#taxlist_train <- taxlist
 
 train_dtm <- process_description(taxlist_train,1)
 #train_dtm <- process_description(taxlist_train, 5)
-crossval_dtm <- process_description(taxlist_crossval, 5)
-test_dtm <- process_description(taxlist_test, 5)
+#crossval_dtm <- process_description(taxlist_crossval, 5)
+#test_dtm <- process_description(taxlist_test, 5)
 
 train_matrix <- join_taxlist_dtm(taxlist_train, train_dtm)
-crossval_matrix <- join_taxlist_dtm(taxlist_crossval, crossval_dtm)
-test_matrix <- join_taxlist_dtm(taxlist_test, test_dtm)
+#crossval_matrix <- join_taxlist_dtm(taxlist_crossval, crossval_dtm)
+#test_matrix <- join_taxlist_dtm(taxlist_test, test_dtm)
 
 train_class <- as.factor(as.matrix(taxlist_train["Classification"]))
-crossval_class <- as.factor(as.matrix(taxlist_crossval["Classification"]))
-test_class <- as.factor(as.matrix(taxlist_test["Classification"]))
+#crossval_class <- as.factor(as.matrix(taxlist_crossval["Classification"]))
+#test_class <- as.factor(as.matrix(taxlist_test["Classification"]))
 
-c50_model <- C5.0(x=train_dtm, y=train_class)
-ripper_model <- JRip(train_class~., data = as.data.frame(train_dtm))
+tree_model <- train(train_matrix, train_class, method = "C5.0")
+rule_model <- train(train_matrix, train_class, method = "JRip")
 
-summary(taxlist_model)
+C5.0.graphviz(c50_model, "c50_model.txt")
+summary(ripper_model)
+ripper_model
 #write(capture.output(summary(taxlist_model)), "c50model.txt")
