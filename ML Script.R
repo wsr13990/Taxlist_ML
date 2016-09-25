06b65eee7c53aa9dc1a79a14758fb3dab83b67fd
WSR13990 <wahyusejatiroso@yahoo.co.id>
2016-09-25 10:13:48 +0800

    Finish transform the description into DTM

--- ML Script.R
+++ ML Script.R
@@ -1,7 +1,7 @@
 library(tm)
 library(SnowballC)
 
-taxlist <- read.csv("C:/ML Taxlist/Taxlist.csv", header = TRUE, sep=";")
+taxlist <- read.csv("D:/Taxlist_ML/Taxlist.csv", header = TRUE, sep=";")
 set.seed(123)
 row_num <- nrow(taxlist)
 train_num <- round(row_num/2)
@@ -12,16 +12,33 @@ crossval_row <- round(nrow(taxlist_test)/2)
 taxlist_crossval <- taxlist_test[1:crossval_row,]
 taxlist_test <- taxlist_test[(crossval_row+1):nrow(taxlist_test),]
 
-description <- taxlist$Deskripsi.Invoice
-description_corpus <- VCorpus(VectorSource(taxlist$Deskripsi.Invoice))
-description_corpus_clean <- tm_map(description_corpus, content_transformer(tolower))
-description_corpus_clean <- tm_map(description_corpus_clean, removePunctuation)
-description_dtm <- DocumentTermMatrix(description_corpus_clean)
+process_description <- function(data, freq){
+  description <- data$Deskripsi.Invoice
+  description_corpus <- VCorpus(VectorSource(data$Deskripsi.Invoice))
+  description_corpus_clean <- tm_map(description_corpus, content_transformer(tolower))
+  description_corpus_clean <- tm_map(description_corpus_clean, removePunctuation)
+  taxlist_dtm <- DocumentTermMatrix(description_corpus_clean)
+  desc_freq_words <- findFreqTerms(taxlist_dtm, freq)
+  taxlist_dtm <- taxlist_dtm[, desc_freq_words]
+  taxlist_dtm <- as.matrix(taxlist_dtm)
+  return(taxlist_dtm)
+}
 
-desc_freq_words <- findFreqTerms(description_dtm, 5)
-description_dtm <- description_dtm[, desc_freq_words]
+join_taxlist_dtm <- function(taxlist, dtm_matrix){
+  matrix <- subset(taxlist, select=-c(Deskripsi.Invoice,Tarif.PPh.SPT,pasal.SPT,Jenis.Jasa))
+  matrix <- cbind(matrix, dtm_matrix)
+  return(matrix)
+}
 
-description_matrix <- as.matrix(description_dtm)
+train_dtm <- process_description(taxlist_train, 5)
+crossval_dtm <- process_description(taxlist_crossval, 5)
+test_dtm <- process_description(taxlist_test, 5)
+
+train_matrix <- join_taxlist_dtm(taxlist_train, train_dtm)
+crossval_matrix <- join_taxlist_dtm(taxlist_crossval, crossval_dtm)
+test_matrix <- join_taxlist_dtm(taxlist_test, test_dtm)
+
+
 
 
 
