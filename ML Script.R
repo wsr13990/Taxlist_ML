b01bfe6dc61d2b6608885caab0ebfec56158e08a
WSR13990 <wahyusejatiroso@yahoo.co.id>
2016-09-25 15:49:45 +0800

    Testing C5.0

--- ML Script.R
+++ ML Script.R
@@ -1,36 +1,49 @@
 library(tm)
 library(SnowballC)
+library(C50)
+library(RWeka)
 
-taxlist <- read.csv("D:/Taxlist_ML/Taxlist.csv", header = TRUE, sep=";")
-set.seed(123)
-row_num <- nrow(taxlist)
-train_num <- round(row_num/2)
-train_sample <- sample(row_num, train_num)
-taxlist_train <- taxlist[train_sample,]
-taxlist_test <- taxlist[-train_sample,]
-crossval_row <- round(nrow(taxlist_test)/2)
-taxlist_crossval <- taxlist_test[1:crossval_row,]
-taxlist_test <- taxlist_test[(crossval_row+1):nrow(taxlist_test),]
+options(java.parameters = "-Xmx8000m")
 
 process_description <- function(data, freq){
   description <- data$Deskripsi.Invoice
   description_corpus <- VCorpus(VectorSource(data$Deskripsi.Invoice))
   description_corpus_clean <- tm_map(description_corpus, content_transformer(tolower))
   description_corpus_clean <- tm_map(description_corpus_clean, removePunctuation)
+  description_corpus_clean <- tm_map(description_corpus_clean, stripWhitespace)
   taxlist_dtm <- DocumentTermMatrix(description_corpus_clean)
   desc_freq_words <- findFreqTerms(taxlist_dtm, freq)
   taxlist_dtm <- taxlist_dtm[, desc_freq_words]
+  taxlist_dtm <- apply(taxlist_dtm, MARGIN=2, convert_count)
   taxlist_dtm <- as.matrix(taxlist_dtm)
   return(taxlist_dtm)
 }
 
 join_taxlist_dtm <- function(taxlist, dtm_matrix){
-  matrix <- subset(taxlist, select=-c(Deskripsi.Invoice,Tarif.PPh.SPT,pasal.SPT,Jenis.Jasa))
+  matrix <- subset(taxlist, select=-c(Deskripsi.Invoice,Tarif.PPh.SPT,pasal.SPT,Jenis.Jasa,Classification,Mata.Uang.DPP))
   matrix <- cbind(matrix, dtm_matrix)
   return(matrix)
 }
 
-train_dtm <- process_description(taxlist_train, 5)
+convert_count <- function(x){
+  x <- ifelse(x, "Yes", "No")
+}
+
+taxlist <- read.csv("D:/Taxlist_ML/Taxlist.csv", header = TRUE, sep=",")
+taxlist <- taxlist[taxlist$Nama.Vendor=="Huawei Tech Investment",]
+set.seed(123)
+#row_num <- nrow(taxlist)
+#train_num <- round(row_num/2)
+#train_sample <- sample(row_num, train_num)
+#taxlist_train <- taxlist[train_sample,]
+#taxlist_test <- taxlist[-train_sample,]
+#crossval_row <- round(nrow(taxlist_test)/2)
+#taxlist_crossval <- taxlist_test[1:crossval_row,]
+#taxlist_test <- taxlist_test[(crossval_row):nrow(taxlist_test),]
+taxlist_train <- taxlist
+
+train_dtm <- process_description(taxlist_train,1)
+#train_dtm <- process_description(taxlist_train, 5)
 crossval_dtm <- process_description(taxlist_crossval, 5)
 test_dtm <- process_description(taxlist_test, 5)
 
@@ -38,11 +51,12 @@ train_matrix <- join_taxlist_dtm(taxlist_train, train_dtm)
 crossval_matrix <- join_taxlist_dtm(taxlist_crossval, crossval_dtm)
 test_matrix <- join_taxlist_dtm(taxlist_test, test_dtm)
 
+train_class <- as.factor(as.matrix(taxlist_train["Classification"]))
+crossval_class <- as.factor(as.matrix(taxlist_crossval["Classification"]))
+test_class <- as.factor(as.matrix(taxlist_test["Classification"]))
 
+c50_model <- C5.0(x=train_dtm, y=train_class)
+ripper_model <- JRip(train_class~., data = as.data.frame(train_dtm))
 
-
-
-
-
-as.character(description_corpus_clean[[1]])
-str(description_dtm)
+summary(taxlist_model)
+#write(capture.output(summary(taxlist_model)), "c50model.txt")
