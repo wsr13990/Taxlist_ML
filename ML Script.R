19eba223af4db5ab6cd7d1b975be3ff2ee238df6
WSR13990 <wahyusejatiroso@yahoo.co.id>
2016-10-25 23:55:18 +0800

    Finished the reasonable desicion rule

--- ML Script.R
+++ ML Script.R
@@ -31,9 +31,9 @@ convert_count <- function(x){
   x <- ifelse(x, "Yes", "No")
 }
 
-taxlist <- read.csv("C:/Taxlist_ML/Taxlist.csv", header = TRUE, sep=",")
-taxlist <- taxlist[taxlist$Nama.Vendor %in% c("Huawei Tech Investment"),]
-# taxlist <- taxlist[taxlist$Nama.Vendor %in% c(
+taxlist <- read.csv("D:/Taxlist_ML/Taxlist.csv", header = TRUE, sep=",")
+taxlist <- taxlist[taxlist$Nama.Vendor %in% c("Tower Bersama"),]
+#taxlist <- taxlist[taxlist$Nama.Vendor %in% c(
 #   "Huawei Tech Investment",
 #   "Ericsson Indonesia",
 #   "Nokia Solutions and Networks Indonesia",
@@ -49,15 +49,15 @@ taxlist <- taxlist[taxlist$Nama.Vendor %in% c("Huawei Tech Investment"),]
 #   "Huawei International Pte Ltd",
 #   "Huawei Technologies Co Ltd"),]
 set.seed(123)
-#row_num <- nrow(taxlist)
-#train_num <- round(row_num/2)
-#train_sample <- sample(row_num, train_num)
-#taxlist_train <- taxlist[train_sample,]
-#taxlist_test <- taxlist[-train_sample,]
-#crossval_row <- round(nrow(taxlist_test)/2)
-#taxlist_crossval <- taxlist_test[1:crossval_row,]
-#taxlist_test <- taxlist_test[(crossval_row):nrow(taxlist_test),]
-taxlist_train <- taxlist
+row_num <- nrow(taxlist)
+train_num <- round(row_num/2)
+train_sample <- sample(row_num, train_num)
+taxlist_train <- taxlist[train_sample,]
+taxlist_test <- taxlist[-train_sample,]
+crossval_row <- round(nrow(taxlist_test)/2)
+taxlist_crossval <- taxlist_test[1:crossval_row,]
+taxlist_test <- taxlist_test[(crossval_row):nrow(taxlist_test),]
+#taxlist_train <- taxlist
 
 train_dtm <- process_description(taxlist_train,1)
 #train_dtm <- process_description(taxlist_train, 5)
@@ -71,14 +71,23 @@ train_matrix <- join_taxlist_dtm(taxlist_train, train_dtm)
 train_class <- as.factor(as.matrix(taxlist_train["Classification"]))
 #crossval_class <- as.factor(as.matrix(taxlist_crossval["Classification"]))
 #test_class <- as.factor(as.matrix(taxlist_test["Classification"]))
+train_matrix <- cbind(train_class,train_matrix)
 
 train_dtm <- as.data.frame(train_dtm)
-Huawei <- C5.0(train_dtm, trials = 5, train_class, rules = TRUE)
+#Huawei <- C5.0(x = train_dtm, , trials = 5, train_class, rules = TRUE)
+#Huawei <- C5.0(train_class~., data = train_matrix[,-c(1,3)], rules = TRUE)
+model <- C5.0(train_dtm, train_class, rules = TRUE, control = C5.0Control(earlyStopping = FALSE))
+#model <- train(train_matrix$train_class~.,
+#               data = train_matrix[,-c(2:4)],
+#               method = "C5.0Cost",
+#               tuneGrid = expand.grid(model = "rule",winnow = FALSE, trials =1, cost = 10))
+
 
 #tree_model <- train(train_matrix, train_class, method = "C5.0")
 #rule_model <- train(train_matrix, train_class, method = "JRip")
 
-summary(Huawei)
+summary(model)
+summary(train_matrix[,c(3)])
 
 #C5.0.graphviz(c50_model, "c50_model.txt")
 #ripper_model
